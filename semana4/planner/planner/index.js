9a8e722d0a23f8c18079742b1d1620f113f481f6
ArturNicolaiewskyPellicioli <arturpellicioli@hotmail.com>
2020-12-08 16:43:14 -0300

    exercicios realizados e desafio 1 feito

---
+++ semana4/planner/planner/index.js
@@ -0,0 +1,25 @@
+const createTask = document.querySelector("#createTask")
+
+
+
+function addTask(){
+    let task = document.querySelector("#tarefa")
+    let taskValue = task.value
+    let days = document.querySelector("#dias-semana")
+    let day = days.value
+    let local = document.getElementById(`${day}`)
+
+
+    if(task.value === ''){
+        alert("Você precisa preencher a tarefa")
+    } else {
+    
+        local.innerHTML += `<li>${taskValue}</li>`
+    
+        task.value = ''
+    }
+}
+
+createTask.addEventListener("click", addTask)
+
+
