8b8c68af580a41fa27fb12c3a0be6a0b2e0b375a
ArturNicolaiewskyPellicioli <arturpellicioli@hotmail.com>
2020-12-11 11:17:58 -0300

    exercício interpretação 1 realizado

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,3 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+// 1- Esse código cria uma função que converte a variavel meuDinheiro em reais, o usuario informa a cotação do dolar pelo prompt, a função conversorDeMoeda pega o parametro, no caso ele tem 100 dolares e multiplica, retornando ao console o resultado.
