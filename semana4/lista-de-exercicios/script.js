3384a0f7a1e6499b3ba391be5f225d70f4c25fe0
ArturNicolaiewskyPellicioli <arturpellicioli@hotmail.com>
2020-12-11 12:52:07 -0300

    exercício logica 1 realizado

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -7,3 +7,24 @@
 // 3- No caso 3 o código separa os números de uma lista em pares e impares em listas diferentes. Criando 3 arrays, um com os numeros e outros vazios, ele usa um laço de repetição para passar em cada item do array e fazer a seguinte pergunta, se o resto da divisão por 2 for 0 (um determinante para numeros pares) ele o colocará no array1, caso contrario colocará no array2 usando .push. Nos consoles temos os 3 arrays sendo mostrados.
 
 // 4- O último caso atribui novos valores às variaveis quando a condição for aceita e imprime no console. Temos um array de numeros variados, e duas variaveis com valores iniciais. o for of cria um looping repetindo a função para cada item do array, e assim sendo, sempre que o numero do array inspecionado for menor que a variavel numero1, o valor é atribuido a variavel. E sempre que for menor ao numero2, o valor é atribuido ao mesmo. No final temos a impressão no console das duas variaveis.
+
+// EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO
+
+// 1- for, for of, forEach
+
+// const lista = ["1", "2", "3"]
+
+// for (let i = 0; i < lista.length; i++){
+// console.log(lista[i])
+// }
+
+// for (numero of lista) {
+//     console.log(numero)
+// }
+
+// let novaLista = lista.map((item, index, array) => {
+
+//     console.log(item)
+
+// })
+
