32accae6e4aa9b9e6e8c5fdbc8c55870c60ee8b2
ArturNicolaiewskyPellicioli <arturpellicioli@hotmail.com>
2020-12-11 11:18:34 -0300

    exercício interpretação 2 realizado

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,3 +1,5 @@
 // EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
 
 // 1- Esse código cria uma função que converte a variavel meuDinheiro em reais, o usuario informa a cotação do dolar pelo prompt, a função conversorDeMoeda pega o parametro, no caso ele tem 100 dolares e multiplica, retornando ao console o resultado.
+
+// 2- Nesse caso o código cria uma função que informa quanto tu vai ganhar investindo em certo tipo de investimento, a função recebe dois parametros, o tipo e o valor investido,usando um swith case ele compara o tipo de investimento que o teste coloca com o tipo de investimento que eles tem nos dados, caso tenha um igual, ele atribui o valorAposInvestimento ao valor inicial vezes a cotação daquele investimento, retornando diferentes resultados no console para cada tipo e valor investidos.
