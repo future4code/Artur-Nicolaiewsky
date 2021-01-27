adc00f57853de813a1ea3b5bf6afe08e47e71812
ArturNicolaiewskyPellicioli <arturpellicioli@hotmail.com>
2021-01-27 16:07:31 -0300

    projeto finalizado

---
+++ semana8/projeto/astromatch/src/Components/Matchs/Styled-Matchs.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Image = styled.img`
+height: 7.5vh;
+width: 7.5vh;
+border-radius: 50%;
+`
+
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 2vh;
+`
+
+export const Main = styled.div`
+    min-height:75vh;
+`
+
+export const Name = styled.p`
+    margin-left: 2vh;
+`
